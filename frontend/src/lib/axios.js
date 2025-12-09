d96ab6eeafab0a4596f27391a7322e3b5ccc8a68
Arun Patidar <161785237+ArunPatidar1267@users.noreply.github.com>
2025-12-09 23:18:18 +0530

    Change BASE_URL to production backend URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://arun-landing-page-backend.up.railway.app";
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}/api`,
